3158133e7fc90d53a33768a34248c6709ebb34a8
hyj11123 <43002611+hyj11123@users.noreply.github.com>
2020-12-08 21:48:35 +0900

    apollo

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import React from "react";
 import ReactDom from "react-dom";
 import App from "./components/App";
+import Client from "./apollo/Client";
+import { ApolloProvider } from "react-apollo-hooks";
 
-ReactDom.render(<App />, document.getElementById("app"));
+ReactDom.render(
+ <ApolloProvider client={Client}>
+  <App />
+ </ApolloProvider>,
+ document.getElementById("app")
+);
